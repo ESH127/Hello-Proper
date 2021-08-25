50e6786370f4dc05f9b2b25c8efac18bd1836bc8
Elijah Harvoy <eharvoy2@huskers.unl.edu>
2021-08-25 01:31:18 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Elijah Harvoy
+ * Date: 8/24/2021
+ * Disc.: hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("hello world!\n");
+     
+     return 0;
+ }
+ 
